50baea2b20ee7a6c2c3b955851f354dce67ff303
pdicerbo <pierluigi.dicerbo@gmail.com>
2016-04-28 12:17:31 +0200

    first commit: first task almost done

---
+++ Day2/Exercise3/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double MyFunc(double x, double x0){
+  return x*x - x0;
+}
+
+double MyDeriv(double x){
+  return 2.*x;
+}
+
+double RN_Method(double x0, int N){
+  int j;
+  double xtmp = x0;
+
+  if(x0 == 0)
+    return x0;
+  
+  for(j = 0; j < N; j++)
+    xtmp = xtmp - MyFunc(xtmp, x0)/MyDeriv(xtmp);
+
+  return xtmp;
+
+}
+
+int main(int argc, char** argv){
+
+  int N = 4;
+  int NPoint = 100;
+  int j;
+
+  double* MyGrid = (double*)malloc(NPoint*sizeof(double));
+  double RealSqrt, MySqrt;
+  double xmin = 0.;
+  double xmax = 20.;
+  double delta = (xmax - xmin)/NPoint;
+
+  // fill MyGrid
+  for(j = 0; j < NPoint; j++)
+    MyGrid[j] = xmin + j*delta;
+
+  FILE* fp;
+  fp = fopen("MyData.dat", "w");
+  
+  for(j = 0; j < NPoint; j++){
+    RealSqrt = sqrt(MyGrid[j]);
+    MySqrt = RN_Method(MyGrid[j], N);
+    fprintf(fp, "%lg\t%lg\t%lg\n", xmin+j*delta, RealSqrt, MySqrt);
+  }
+  fclose(fp);
+  
+  return 0;
+}
